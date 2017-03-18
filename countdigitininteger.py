802a4adb34ff9983ef4e73fa7d944913b4c2e450
M.sowmiya <sowmiya21295@gmail.com>
2017-03-18 22:21:06 +0530

    Update countdigitininteger.py

--- countdigitininteger.py
+++ countdigitininteger.py
@@ -1,6 +1,11 @@
 i=input("enter the value")
     count=0
-    while(i!=0):
-        i=i/10
-        count=count+1
+    if(i<0):
+        print("enter positive  integer")
+    else:
+        while(i!=0):
+            i=i/10
+            count=count+1
     print"count=",count
+
+
