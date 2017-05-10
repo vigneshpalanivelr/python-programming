c88af39302809e77432c40cddbc4f526fea5adc4
M.sowmiya <sowmiya21295@gmail.com>
2017-05-10 21:23:05 +0530

    Create eveninterval.py

---
+++ eveninterval.py
@@ -0,0 +1,12 @@
+ a=input("Enter the first interval")
+    b=input("enter the second interval")
+    if(a%2==0):
+        a=a+2
+        while(a<b):
+            print"printng", a
+            a=a+2
+    else:
+        a=a+1
+        while(a<b):
+            print"printng", a
+            a=a+2
