c29fe45fbd550cccd03fe4e753179b725e49bbf1
M.sowmiya <sowmiya21295@gmail.com>
2017-05-10 21:29:28 +0530

    Update eveninterval.py

--- eveninterval.py
+++ eveninterval.py
@@ -1,12 +1,16 @@
- a=input("Enter the first interval")
+a=input("Enter the first interval")
     b=input("enter the second interval")
-    if(a%2==0):
-        a=a+2
-        while(a<b):
-            print"printng", a
+    if(a>0):
+        if(a%2==0):
             a=a+2
+            while(a<b):
+                print"printng", a
+                a=a+2
+        else:
+            a=a+1
+            while(a<b):
+                print"printng", a
+                a=a+2
     else:
-        a=a+1
-        while(a<b):
-            print"printng", a
-            a=a+2
+        print"type positive input"
+
