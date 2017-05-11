d759c7c54e5944beda9d9514f241082f76da6e64
M.sowmiya <sowmiya21295@gmail.com>
2017-05-11 20:19:19 +0530

    Update primeinterval.py

--- primeinterval.py
+++ primeinterval.py
@@ -1,9 +1,12 @@
     a=input("Enter the first interval")
     b=input("enter the second interval")
-    for num in range(a,b):
-        a=a+1
-        for i in range(2,3):
-            if num%i==0:
-                break
-            else:
-                print num," is a primenumber"
+    if(a>0):
+        for num in range(a,b):
+            a=a+1
+            for i in range(2,3):
+                if num%i==0:
+                    break
+                else:
+                    print num," is a primenumber"
+    else:
+        print"positive input only"
