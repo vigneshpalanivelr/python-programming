c19e2f3f2baa7de7f0b694981283fa3bb14bd165
M.sowmiya <sowmiya21295@gmail.com>
2017-05-12 10:51:20 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,6 +1,9 @@
-a=input("Enter the number")
+    a=input("Enter the number")
     b=a-1
-    while(b>0):
-        a=a*b
-        b=b-1
-    print"factorial",a
+    if(a>0):
+        while(b>0):
+            a=a*b
+            b=b-1
+        print"factorial",a
+    else:
+        print"Enter positiive only"
