b7bbaaa50418bad21ac3673893a649195240d5e0
M.sowmiya <sowmiya21295@gmail.com>
2017-05-12 12:07:06 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,7 +1,10 @@
     a=input("Enter the number")
     rev=0
-    while(a>0):
-        rem=a%10
-        rev=rev*10+rem
-        a=a/10
-    print rev
+    if(a>0):
+        while(a>0):
+            rem=a%10
+            rev=rev*10+rem
+            a=a/10
+        print rev
+    else:
+        print("positive input only")
