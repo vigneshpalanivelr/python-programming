6db9b6084805ed2aba46e52b8d683fdcedb2582c
M.sowmiya <sowmiya21295@gmail.com>
2017-05-17 10:00:14 +0530

    Update and rename nega&posreverse.py to negandposreverse.py

---
+++ negandposreverse.py
@@ -0,0 +1,15 @@
+a=input("Enter the input")
+    rev=0
+    if(a>0):
+        while(a>0):
+            rem=a%10
+            rev=rev*10+rem
+            a=a/10
+        print rev
+    else:
+        a=a*-1
+        while(a>0):
+            rem=a%10
+            rev=rev*10+rem
+            a=a/10
+        print rev*-1
