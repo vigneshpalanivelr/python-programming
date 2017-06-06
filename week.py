8865eb4b917122cf2b15774faa926333c603d376
M.sowmiya <sowmiya21295@gmail.com>
2017-06-06 10:35:27 +0530

    Update week.py

--- week.py
+++ week.py
@@ -1,6 +1,6 @@
 x=input("Enter the string to  check working day or not")
-    a="monday","tuesday","wed","thus","fri"
-    b="sat","sun"
+    a="monday","tuesday","wednesday","thursday","friday"
+    b="saturday","sunday"
     if(x in a):
         print"workingday(",x,")->true"
     else:
