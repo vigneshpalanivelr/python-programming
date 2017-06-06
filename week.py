b7885260046ea4e93443f1e3de7a08596c609bd1
M.sowmiya <sowmiya21295@gmail.com>
2017-06-06 10:32:32 +0530

    Create week.py

---
+++ week.py
@@ -0,0 +1,7 @@
+x=input("Enter the string to  check working day or not")
+    a="monday","tuesday","wed","thus","fri"
+    b="sat","sun"
+    if(x in a):
+        print"workingday(",x,")->true"
+    else:
+        print"workingday(",x,")->false"
