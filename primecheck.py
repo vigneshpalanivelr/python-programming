6e48c72e474012166c67aa8884263961b00f6feb
M.sowmiya <sowmiya21295@gmail.com>
2019-01-11 11:40:38 +0530

    Create primecheck.py

---
+++ primecheck.py
@@ -0,0 +1,8 @@
+a,b1= [int(x) for x in input().split()]  
+for num in range(a,b1+1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num)
